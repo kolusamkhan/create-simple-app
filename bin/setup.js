71e258ba9ed83fcb33fbe87f52bb83a394e23fef
kolusam <kolusamkhan@gmail.com>
2022-05-14 12:52:36 +0400

    updated to remove fixup script

--- bin/setup.js
+++ bin/setup.js
@@ -59,7 +59,7 @@ async function setup() {
     appendFileSync(".gitignore", "\r\n.env", "utf8");
 
     /** remove extra files and folders from disk. we don't need it anymore */
-    unlinkSync(join(process.cwd(), "quick-start.gif"));
+    unlinkSync(join(process.cwd(), "fixup"));
     unlinkSync(join(process.cwd(), "CHANGELOG.md"));
     unlinkSync(join(process.cwd(), "README.md"));
     unlinkSync(join(process.cwd(), "bin", "setup.js"));
